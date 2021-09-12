bff281fc7b58d5125304d9dfe769485213e505bf
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-09-12 07:25:45 -0300

    basic config on the package.json for webpack

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//to do app
+// start from Data to Interface (GUI);
+
+
+/* TODO SECTION*/
+//gonna generate (Create) single todo (obj)
+//// each obj gonna have (title, description, dueDate and priority.)
+// todo gonna have date, need to find way to work with date
+
+// gonna have to have projects, each project gonna have it's own todos
+// has to have default todo
+
+// have the option to set a todo as complete and be able to change the todo priority
+
+
+/* STORAGE SECTION*/
+//save and retrieve todo
+//modify singe todo edit;
+//modify project when todo is edit, removed or added;
+
+
+/* DISPLAY SECTION*/
+// view all projects
+//// view todo in each project
+//// expand single todo
+//// delete a todo
